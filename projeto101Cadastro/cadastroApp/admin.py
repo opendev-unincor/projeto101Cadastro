bdc840753a5836c4e0943587662405aa9a713e82
rodrigo luciano costa <rodrigolucianocosta@hotmail.com>
2019-07-20 10:13:59 -0300

    atualizaçao

--- projeto101Cadastro/cadastroApp/admin.py
+++ projeto101Cadastro/cadastroApp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from . models import Pessoa
 # Register your models here.
+
+class PessoaAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    save_as = True
+
+admin.site.register(Pessoa,PessoaAdmin)
--- projeto101Cadastro/cadastroApp/models.py
+++ projeto101Cadastro/cadastroApp/models.py
@@ -2,7 +2,4 @@ from django.db import models
 
 # Create your models here.
 class Pessoa(models.Model):
-    name = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.name
+	nome = models.CharField('nome',max_length=50)
